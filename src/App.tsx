68f1ae383d65405182b375a3960577b4fe30bc69
afernando <ak915893@dal.ca>
2025-06-15 15:02:25 -0300

    product detail page added. header/footer modified to fit prototype.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./pages/login";
 import Signup from './pages/signup';
+import ProductDetails from './pages/product-details';
 
 function App() {
-
-
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/product-details" element={<ProductDetails/>} />
       </Routes>
     </BrowserRouter>
   )
